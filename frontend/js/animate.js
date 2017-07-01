3ce0e9a472e847b3d54bf097cf6f6275e9a41526
NightScap3 <baran.peters@lessing-ffm.net>
2017-07-01 12:02:12 +0200

    Added blue shadow to buttons

--- frontend/js/animate.js
+++ frontend/js/animate.js
@@ -4,4 +4,24 @@ $("#twtContainer").hover(function(){
     }, function(){
         $('#twtContainer').removeClass('shadow-blue')
 });
+$("#button1").hover(function(){
+    $('#button1').addClass('shadow-blue');
+    }, function(){
+        $('#button1').removeClass('shadow-blue')
+});
+$("#button2").hover(function(){
+    $('#button2').addClass('shadow-blue');
+    }, function(){
+        $('#button2').removeClass('shadow-blue')
+});
+$("#button3").hover(function(){
+    $('#button3').addClass('shadow-blue');
+    }, function(){
+        $('#button3').removeClass('shadow-blue')
+});
+$("#button4").hover(function(){
+    $('#button4').addClass('shadow-blue');
+    }, function(){
+        $('#button4').removeClass('shadow-blue')
+});
 });
