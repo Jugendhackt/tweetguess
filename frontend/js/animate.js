bf8a2555deb78fcf3aaf598eabc963d0976f4f7c
NightScap3 <baran.peters@lessing-ffm.net>
2017-07-01 12:03:41 +0200

    changed to grey shadow

--- frontend/js/animate.js
+++ frontend/js/animate.js
@@ -1,27 +1,27 @@
 $( document ).ready(function() {
 $("#twtContainer").hover(function(){
-    $('#twtContainer').addClass('shadow-blue');
+    $('#twtContainer').addClass('shadow');
     }, function(){
-        $('#twtContainer').removeClass('shadow-blue')
+        $('#twtContainer').removeClass('shadow')
 });
 $("#button1").hover(function(){
-    $('#button1').addClass('shadow-blue');
+    $('#button1').addClass('shadow');
     }, function(){
-        $('#button1').removeClass('shadow-blue')
+        $('#button1').removeClass('shadow')
 });
 $("#button2").hover(function(){
-    $('#button2').addClass('shadow-blue');
+    $('#button2').addClass('shadow');
     }, function(){
-        $('#button2').removeClass('shadow-blue')
+        $('#button2').removeClass('shadow')
 });
 $("#button3").hover(function(){
-    $('#button3').addClass('shadow-blue');
+    $('#button3').addClass('shadow');
     }, function(){
-        $('#button3').removeClass('shadow-blue')
+        $('#button3').removeClass('shadow')
 });
 $("#button4").hover(function(){
-    $('#button4').addClass('shadow-blue');
+    $('#button4').addClass('shadow');
     }, function(){
-        $('#button4').removeClass('shadow-blue')
+        $('#button4').removeClass('shadow')
 });
 });
