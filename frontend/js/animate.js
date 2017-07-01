ee38f452d94894ee086ef5960590703b49c254b2
Moritz Beck <blitze299@users.noreply.github.com>
2017-07-01 18:31:02 +0200

    timer finish

--- frontend/js/animate.js
+++ frontend/js/animate.js
@@ -27,15 +27,45 @@ $('.buttonz').click(function() {
     }, 1000);
 });
 
-function timer(){
-  var start = "60";
-  while (start > 0) {
-    start = start - 1;
-    sleep(1000);
-    return start;
-  }
+function startTimer(duration, display) {
+    var start = Date.now(),
+        diff,
+        minutes,
+        seconds;
+    function timer() {
+        // get the number of seconds that have elapsed since
+        // startTimer() was called
+        diff = duration - (((Date.now() - start) / 1000) | 0);
+
+        // does the same job as parseInt truncates the float
+        minutes = (diff / 60) | 0;
+        seconds = (diff % 60) | 0;
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (diff <= 0) {
+            // add one second so that the count down starts at the full duration
+            // example 05:00 not 04:59
+            start = Date.now() + 1000;
+        }
+    };
+    // we don't want to wait a full second before the timer starts
+    timer();
+    setInterval(timer, 1000);
+}
+
+window.onload = function () {
+    var fiveMinutes = 60 * 0.5,
+        display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
 };
 
+
+
 $(document).ready(function(){
-  $('#score').text("999");
+  $('#score').text("0");
+
 });
