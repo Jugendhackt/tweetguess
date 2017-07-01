73b2522693c1756ceaceff3cbc24dde60255bcb5
Moritz Beck <blitze299@users.noreply.github.com>
2017-07-01 16:31:28 +0200

    new animate

--- frontend/js/animate.js
+++ frontend/js/animate.js
@@ -1,1 +1,46 @@
 
+/*function validate(i){
+    $.post("",
+   {
+       name: i,
+   },
+   function(data, status){
+       var a = ["answer1", "answer2", "answer3", "answer4"];
+       $.each(a, function(i, item) {
+         if($(item).html() == data){
+             // wird richtiges nicht verblasse
+             $(item).html()
+             if($(item).html() == i){
+             }
+         } else {
+             //verblassen
+             $(item)
+         }
+ });
+   });
+} */
+
+function valitest(i){
+    var a = ["#answer1", "#answer2", "#answer3", "#answer4"];
+    var b = ["#panel1", "#panel2", "#panel3", "#panel4"];
+    $.each(a, function(i, item) {
+      if($(item).html() == "Button 1"){
+          $( b[i] ).animate({
+          backgroundColor: "#00ff00",
+          color: "#000",
+        }, 1000 );
+          if($(item).html() == i){
+              console.log("right!");
+          } else{
+              console.log("false!");
+          }
+      } else {
+          //verblassen
+          $(item).fadeTo("fast", 0.4);
+          $( b[i] ).animate({
+          backgroundColor: "#ff0000",
+          color: "#000",
+        }, 1000 );
+      }
+});
+}
